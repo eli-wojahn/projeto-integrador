0fa73065d0b4071deb89f37c931cd1d23e36a372
Elias Wojahn <elias.wojahn@bridge-mt.com>
2023-10-23 20:31:45 -0300

    altera campo e adiciona tooltip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import {
   SidebarContainer, Button, Image, ImageIcons, FloatingButton,
   BackgroundImage, PriorityContainer, ButtonsContainer, IconButton, ButtonRow, ImagePins
 } from './HomeStyle';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import polaroidImage from './polaroid.png';
 import share1 from './share.png';
@@ -17,33 +18,45 @@ import pin_green from './pin_green.png';
 import pin_yellow from './pin_yellow.png';
 import pin_purple from './pin_purple.png';
 
-const EditableForm = ({ initialValue }) => {
+const PriorityField = ({ id, initialValue }) => {
   const [text, setText] = useState(initialValue);
 
-  const handleChange = (e) => {
-    setText(e.target.value);
+  const updatePriority = async () => {
+    try {
+      await axios.put(`http://localhost:3001/prioridades/${id}`, { nome: text });
+    } catch (error) {
+      console.error('Erro ao atualizar a prioridade:', error);
+    }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  useEffect(() => {
+    if (initialValue !== text) {
+      updatePriority();
+    }
+  }, [text, initialValue, id]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={text}
-        onChange={handleChange}
-        style={{
-          backgroundColor: '#f0f0f0',
-          border: 'none',
-          outline: 'none',
-          width: '100%',
-          fontSize: '16px',
-          padding: '5px',
-        }}
-      />
-    </form>
+    <input
+      type="text"
+      value={text}
+      onChange={(e) => {
+        // Limitar a 15 caracteres
+        if (e.target.value.length <= 15) {
+          setText(e.target.value);
+        }
+      }}
+      onBlur={updatePriority}
+      maxLength={15}
+      title="Customize aqui o nome da prioridade"
+      style={{
+        backgroundColor: '#f0f0f0',
+        border: 'none',
+        outline: 'none',
+        width: '100%',
+        fontSize: '16px',
+        padding: '5px',
+      }}
+    />
   );
 };
 
@@ -68,13 +81,12 @@ const HomeScreen = () => {
         return {
           name: document.getElementById('swal-input1').value,
           imageURL: document.getElementById('swal-input2').value,
-          url: document.getElementById('swal-input3').value
+          url: document.getElementById('swal-input3').value,
         };
-      }
+      },
     }).then((result) => {
       if (result.isConfirmed) {
         console.log('Result:', result.value);
-        // TODO: Add logic to save the entered values
       }
     });
   };
@@ -85,23 +97,23 @@ const HomeScreen = () => {
       <Button>Organizar</Button>
       <PriorityContainer>
         <ImagePins src={pin_red} alt="Imagem" />
-        <EditableForm initialValue="Prioridade 1" />
+        <PriorityField initialValue="Prioridade 1" />
       </PriorityContainer>
       <PriorityContainer>
         <ImagePins src={pin_blue} alt="Imagem" />
-        <EditableForm initialValue="Prioridade 2" />
+        <PriorityField initialValue="Prioridade 2" />
       </PriorityContainer>
       <PriorityContainer>
         <ImagePins src={pin_green} alt="Imagem" />
-        <EditableForm initialValue="Prioridade 3" />
+        <PriorityField initialValue="Prioridade 3" />
       </PriorityContainer>
       <PriorityContainer>
         <ImagePins src={pin_purple} alt="Imagem" />
-        <EditableForm initialValue="Prioridade 4" />
+        <PriorityField initialValue="Prioridade 4" />
       </PriorityContainer>
       <PriorityContainer>
         <ImagePins src={pin_yellow} alt="Imagem" />
-        <EditableForm initialValue="Prioridade 5" />
+        <PriorityField initialValue="Prioridade 5" />
       </PriorityContainer>
       <ButtonsContainer style={{ marginTop: 'auto' }}>
         <ButtonRow>
@@ -117,7 +129,12 @@ const HomeScreen = () => {
             <ImageIcons src={avatar} alt="avatar" />
           </IconButton>
           <IconButton>
-            <ImageIcons src={exit} onClick={handleExitClick} alt="exit" style={{ width: '50px' }} />
+            <ImageIcons
+              src={exit}
+              onClick={handleExitClick}
+              alt="exit"
+              style={{ width: '50px' }}
+            />
           </IconButton>
         </ButtonRow>
       </ButtonsContainer>
