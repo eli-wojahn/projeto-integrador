1274aeb70c274a29aa74b23bd3ca2234b8c4cf6f
Elias Wojahn <elias.wojahn@bridge-mt.com>
2023-11-18 20:29:15 -0300

    Adiciona criação de prioridades

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import * as images from './Images';
+
 import PriorityField from './PriorityField';
 import {
   SidebarContainer,
@@ -13,31 +15,77 @@ import {
   ImageIcons
 } from './HomeStyle';
 
-const Sidebar = ({ handleExitClick, openModal }) => {
+const Sidebar = ({ handleExitClick }) => {
+  const [prioridades, setPrioridades] = useState([]);
+
+  const fetchPrioridades = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/prioridades');
+      const prioridadeData = response.data;
+
+      setPrioridades(prioridadeData);
+    } catch (error) {
+      console.error('Error fetching prioridade data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPrioridades();
+  }, []);
+
+  const corPorId = {
+    1: 'red',
+    2: 'blue',
+    3: 'green',
+    4: 'purple',
+    5: 'yellow',
+  };
+
+  const CreatePriority = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/prioridades', {
+        nome: 'Prioridade',
+      });
+
+      const createdPriority = response.data;
+      setPrioridades([...prioridades, createdPriority]);
+    } catch (error) {
+      console.error('Error creating priority:', error);
+    }
+  };
+
+  const UpdatePriority = async (id, newName) => {
+    try {
+      await axios.put(`http://localhost:3001/prioridades/${id}`, {
+        nome: newName,
+      });
+
+      const updatedPriorities = prioridades.map((priority) =>
+        priority.id === id ? { ...priority, nome: newName } : priority
+      );
+
+      setPrioridades(updatedPriorities);
+    } catch (error) {
+      console.error('Error updating priority:', error);
+    }
+  };
+
   return (
     <SidebarContainer>
       <Image src={images.polaroidImage} alt="Imagem" />
       <Button>Organizar</Button>
-      <PriorityContainer>
-        <ImagePins src={images.pin_red} alt="Imagem" />
-        <PriorityField initialValue="Prioridade 1" />
-      </PriorityContainer>
-      <PriorityContainer>
-        <ImagePins src={images.pin_blue} alt="Imagem" />
-        <PriorityField initialValue="Prioridade 2" />
-      </PriorityContainer>
-      <PriorityContainer>
-        <ImagePins src={images.pin_green} alt="Imagem" />
-        <PriorityField initialValue="Prioridade 3" />
-      </PriorityContainer>
-      <PriorityContainer>
-        <ImagePins src={images.pin_purple} alt="Imagem" />
-        <PriorityField initialValue="Prioridade 4" />
-      </PriorityContainer>
-      <PriorityContainer>
-        <ImagePins src={images.pin_yellow} alt="Imagem" />
-        <PriorityField initialValue="Prioridade 5" />
-      </PriorityContainer>
+
+      <Button onClick={CreatePriority}>Adicionar Prioridade</Button>
+      {prioridades.map((prioridade, index) => (
+        <PriorityContainer key={index}>
+          <ImagePins src={images[`pin_${corPorId[prioridade.id]}`]} alt="Imagem" />
+          <PriorityField
+            initialValue={`${prioridade.nome} ${prioridade.id}`}
+            onUpdate={(newName) => UpdatePriority(prioridade.id, newName)}
+          />
+        </PriorityContainer>
+      ))}
+
       <ButtonsContainer style={{ marginTop: 'auto' }}>
         <ButtonRow>
           <IconButton>
